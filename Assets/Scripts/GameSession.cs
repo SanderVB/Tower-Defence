961d30508754cb3703cd25812957b745a7c079bc
Sander van Beek <vanbeekster@gmail.com>
2019-02-07 23:12:38 +0100

    implemented ring buffer

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField] int playerHealth = 3, towerLimit = 5;
+    int score;
+
+    private void Awake()
+    {
+        int gameSessionCount = FindObjectsOfType<GameSession>().Length;
+        if (gameSessionCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public bool GetTurretAllowance()
+    {
+        return FindObjectsOfType<TowerController>().Length < towerLimit; //count number of active towers and give permission to place another one if it's below the set limit
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+}
