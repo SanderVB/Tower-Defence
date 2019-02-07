110cbd18a9928698fceea8917253b9be9ff113cf
Sander van Beek <vanbeekster@gmail.com>
2019-02-07 14:08:32 +0100

    Added towers

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform target;
+
+    private void Update()
+    {
+        objectToPan.LookAt(target);
+    }
+
+}
